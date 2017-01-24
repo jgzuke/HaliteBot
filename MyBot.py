100b5eb7f4bcc859ed51965db62af7fd14ee6d6a
jgzuke <jgzuke@gmail.com>
2017-01-23 22:53:37 -0500

    refactor logic into reward function

--- MyBot.py
+++ MyBot.py
@@ -5,37 +5,41 @@ import logging
 import numpy as np
 logging.basicConfig(level=logging.DEBUG, filename="logfile", filemode="a+",
                     format="%(asctime)-15s %(levelname)-8s %(message)s")
+logging.info('Log Start')
 
 myID, game_map = hlt.get_init()
 hlt.send_init("MyPythonBot")
 
-def get_random_momentum():
-    momentum = [NORTH, EAST, SOUTH, WEST]
-    random.shuffle(momentum)
-    return momentum
-
-momentum_map = [[get_random_momentum() for i in range(30)] for j in range(30)]
 # Frame returned as list of Square(x=0, y=0, owner=0, strength=93, production=2)
+actions = [NORTH, EAST, SOUTH, WEST, STILL]
+action_directions = ((0, -1), (1, 0), (0, 1), (-1, 0), (0, 0))
+def get_move_advantage(square, action):
+    if action == STILL:
+        return 1
 
-def add_movement_to_momentum(square, direction):
-    momentum = momentum_map[square.x][square.y]
-    momentum.insert(0, momentum.pop(momentum.index(direction)))
+    neighbor = game_map.get_target(square, action)
+    # Unowned Space
+    if neighbor.owner == 0:
+        if neighbor.strength < square.strength and neighbor.strength > 0:
+            return 4
+        else:
+            return 0
 
-movements = ((0, -1), (1, 0), (0, 1), (-1, 0))
+    # Enemy Space
+    if neighbor.owner != myID:
+        if neighbor.strength < square.strength:
+            return 3
+        else:
+            return 0
+
+    # My Space
+    if square.strength > neighbor.strength > 1.5 and square.strength > 100:
+        return 2
+    else:
+        return 0
+
 def assign_move(square):
-    for direction in momentum_map[square.x][square.y]:
-        neighbor = game_map.get_target(square, direction)
-        if neighbor.owner != myID and neighbor.strength < square.strength:
-            add_movement_to_momentum(square, direction)
-            return Move(square, direction)
-
-    for direction in momentum_map[square.x][square.y]:
-        neighbor = game_map.get_target(square, direction)
-        if neighbor.owner == myID and square.strength > neighbor.strength > 1.5 and square.strength > 100:
-            add_movement_to_momentum(square, direction)
-            return Move(square, direction)
-
-    return Move(square, STILL)
+    return Move(square, actions[np.argmax([get_move_advantage(square, action) for action in actions])])
 
 while True:
     game_map.get_frame()
